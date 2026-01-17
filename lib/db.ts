49e0d736ee1c41e45a29a67dd576631240928311
Alush Pranav <alushpranav@gmail.com>
2026-01-17 20:46:39 +0530

    fixed db_url in prod

--- lib/db.ts
+++ lib/db.ts
@@ -1,3 +1,7 @@
 import postgres from "postgres";
 
-export const sql = postgres(process.env.DATABASE_URL!);
+const sql = postgres(process.env.DATABASE_URL!, {
+  ssl: process.env.NODE_ENV === "production" ? "require" : false,
+});
+
+export default sql;
